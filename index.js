175b9d19b2704204968361670d6c10ac76734f56
aleanrocha <ale.oficial.me@gmail.com>
2023-12-23 22:42:45 -0300

    query params

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+/*
+  - Query params => site.com/users?name=abreu&age=30 - FILTRO
+*/
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 // criar rota
 app.get('/users', (request, response) => {
-  return response.send('Hello NodeJS')
+  // const name = request.query.name
+  // const age = request.query.age
+
+  const {name, age} = request.query // destructuring assignment
+
+  console.log(request)
+
+  return response.json({ name, age })
+
+  // return response.send('Hello NodeJS')
 })
 
 // criar porta
